e94e2e1415d3693f508f8d6be880ba2190949ade
Vadim Lilianov <prophetdt670@gmail.com>
2016-04-29 17:47:00 +0300

    NaN exep
    Типа обработчик если ввели не число

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -4,21 +4,24 @@ exports.home = function (req, res) {
     , message: 'This is the "home" action of "pages" controller'
   })
 }
+//Обработчик данных с формы
 exports.calc = function (req, res) {
 	var first=req.body.first,
 	second = req.body.second;
-	if (isNumeric(first) && isNumeric(second))
+	//Проверяем на число и узнаем что ж хотели сделать
+	if (isNumeric(first) && isNumeric(second)) {
 		if (req.body.operation == "minus")
 			a=+first - +second;
 		else
-			a=+first + +second;
+	a=+first + +second;}
+	else a="Вы ввели не число";
 		res.render('pages/calc', {
 			title: "Результат",
 			message: a
 			});
 	console.log(a);
 }
-
+//Проверка на число
 isNumeric = function (n){
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
